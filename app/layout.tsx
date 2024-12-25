60b61b7f3d2090ce3bba69d80be4c945393191e9
yaswanth <yaswanth@getpatronus.com>
2024-12-25 19:59:13 +0530

    added theme color

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,8 @@ export const metadata: Metadata = {
     apple: [
       { url: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext x='35%25' y='50%25' dominant-baseline='central' text-anchor='middle' font-size='80' font-weight='bold' fill='%2300ffff'%3ED%3C/text%3E%3Ctext x='65%25' y='50%25' dominant-baseline='central' text-anchor='middle' font-size='80' font-weight='bold' fill='white'%3ES%3C/text%3E%3C/svg%3E", type: "image/svg+xml" }
     ],
-  }
+  },
+  themeColor: '#000000'
 };
 
 export default function RootLayout({
