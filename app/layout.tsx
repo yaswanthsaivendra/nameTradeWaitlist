217225950124bf46bab9f1dbc8fe7c733f6340cd
yaswanth <yaswanth@getpatronus.com>
2024-12-25 19:27:26 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,23 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Montserrat } from "next/font/google";
-import Head from "next/head";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "DomSell",
-  description: "",
+  description: "Buy, Sell and Manage Domains",
+  icons: {
+    icon: [
+      { 
+        url: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext x='35%25' y='50%25' dominant-baseline='central' text-anchor='middle' font-size='80' font-weight='bold' fill='%2300ffff'%3ED%3C/text%3E%3Ctext x='65%25' y='50%25' dominant-baseline='central' text-anchor='middle' font-size='80' font-weight='bold' fill='white'%3ES%3C/text%3E%3C/svg%3E",
+        type: "image/svg+xml"
+      }
+    ],
+    apple: [
+      { url: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext x='35%25' y='50%25' dominant-baseline='central' text-anchor='middle' font-size='80' font-weight='bold' fill='%2300ffff'%3ED%3C/text%3E%3Ctext x='65%25' y='50%25' dominant-baseline='central' text-anchor='middle' font-size='80' font-weight='bold' fill='white'%3ES%3C/text%3E%3C/svg%3E", type: "image/svg+xml" }
+    ],
+  }
 };
 
 export default function RootLayout({
@@ -17,9 +27,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={montserrat.className}>
-      <Head>
-        <meta name="theme-color" content="#000000" />
-      </Head>
       <body>{children}</body>
     </html>
   );
